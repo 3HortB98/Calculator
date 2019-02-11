8ed2230b4aa26c33aea9aa2e1ea56cb5a05ab898
Ben H <benhorton16@gmail.com>
2019-02-11 13:02:18 +0000

    external storage added+ permission required

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.calculator;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.constraint.Group;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -36,6 +42,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btnEditName;
 
     private List<String> log = new ArrayList<>();
+
+    private boolean isPermissionRequestInProgress = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -193,13 +201,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void handleLogClick(){
         if (log.size()>0) {
-            writeLogToFile();
+            //writeLogToFile();
+            writeLogsToExternalStorage();
         }
         Intent intent = new Intent(MainActivity.this, LogsActivity.class);
         intent.putStringArrayListExtra("LogsResult", (ArrayList<String>)log);
         Room room = new Room("Training Room", 6);
         intent.putExtra("Room", room);
-        startActivity(intent);
+        if(!isPermissionRequestInProgress) {
+            startActivity(intent);
+        }
     }
 
     private void readNameFromSharedPrefences(){
@@ -209,7 +220,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             nameGrp.setVisibility(View.GONE);
 
         }
-        tvResult.setText("Welcome" + name);
+        tvResult.setText("Welcome " + name);
         btnEditName.setVisibility(View.VISIBLE);
     }
 
@@ -236,5 +247,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             ioException.printStackTrace();
         }
     }
+    private void writeLogsToExternalStorage(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            requestStoragePermission();
+        }
+    }
 
+    private void requestStoragePermission() {
+        isPermissionRequestInProgress = true;
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},100);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 100 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            //isPermissionRequestInProgress = false;
+            writeFile();
+            //isPermissionRequestInProgress=false;
+        }
+    }
+
+    private void writeFile() {
+        if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+            File parentFolder = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS),"Calculator");
+            parentFolder.mkdirs();
+
+            File file = new File(parentFolder, "Logs.txt");
+
+            try (FileOutputStream fileOutputStream = new FileOutputStream(file)){
+                StringBuilder stringBuilder = new StringBuilder();
+                for (String result : log){
+                    stringBuilder.append(result);
+                    stringBuilder.append("\n");
+                }
+                fileOutputStream.write(stringBuilder.toString().getBytes());
+
+            } catch (IOException ioException){
+                Toast.makeText(this,"File Not Found", Toast.LENGTH_SHORT).show();
+                ioException.printStackTrace();
+            }
+
+        }
+    }
 }
